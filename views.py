dae7af2d100d33517cea054938acf30052ad1590
unknown <shami@.(none)>
2010-06-20 14:23:11 +0300

    Init

---
+++ views.py
@@ -0,0 +1,5 @@
+from models import User
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm

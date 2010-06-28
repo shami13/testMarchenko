b6f2f9a25cf36fb147398a44036aa1ee183446b1
unknown <shami@.(none)>
2010-06-28 22:09:01 +0300

    add middleware.py and change work with database

---
+++ middleware.py
@@ -0,0 +1,6 @@
+from models import URL
+class MiddlewareHttp():
+    def process_request(self, request):
+        url = URL()
+        url.url = request.get_host() + request.get_full_path()
+        url.save()
--- settings.py
+++ settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'testMarchenko.middleware.MiddlewareHttp',
 )
 
 ROOT_URLCONF = 'testMarchenko.urls'

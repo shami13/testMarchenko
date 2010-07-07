c1b69d8c097da461ddabf60ff0d0f4e8cfde16c4
unknown <shami@.(none)>
2010-07-07 11:51:58 +0300

    fix tests

--- settings.py
+++ settings.py
@@ -65,7 +65,8 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.load_template_source',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = ('testMarchenko.context_processors.add_django_settings_to_request',)
+TEMPLATE_CONTEXT_PROCESSORS = ('django.core.context_processors.auth',
+                               'testMarchenko.context_processors.add_django_settings_to_request',)
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',

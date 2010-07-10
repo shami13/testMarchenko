a4fb929969e2ca235a39ef715abac2cbeb7a392f
unknown <shami@.(none)>
2010-07-10 14:00:27 +0300

    add tests

--- tests.py
+++ tests.py
@@ -7,20 +7,28 @@ from models import URL
 class SimpleTest(TestCase):
     def test_details(self):
         client = Client()
-        response = client.get('/main/')
+        client.post('/accounts/login/', {'username' : 'admin', 'password':'password'})
+        response = client.get('')
         self.failUnlessEqual(response.status_code, 200)
         self.failIfEqual(response.context['form'], None, "No customer in context")
     
     def test_middleware(self):
         count = URL.objects.count()
         client = Client()
-        client.get('/main/')
+        client.get('')
         count = URL.objects.count() - count
         self.failIfEqual(count, 0, "Middleware don't work")
         url=URL.objects.latest('pk')
-        self.failUnlessEqual(url.url, 'testserver/main/', "Middleware don't work")
+        self.failUnlessEqual(url.url, 'testserver/', "Middleware don't work")
         
     def test_context_processor(self):
         client = Client()
-        response = client.get('/main/')
+        client.post('/accounts/login/', {'username' : 'admin', 'password':'password'})
+        response = client.get('')
         self.failIfEqual(response.context['django.settings'], None, "Context processor don't work")
+        
+    def test_auth(self):
+        client = Client()
+        response = client.get('')
+        self.failUnlessEqual(response.status_code, 302)
+    

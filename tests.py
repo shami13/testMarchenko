3ae81409f82c6634869891bf7c6d3f02745957f3
unknown <shami@.(none)>
2010-07-18 00:13:12 +0300

    add tests of ticket 9

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 import os
+from management.commands.printmodels import print_modles
+from django.db import models
+import string
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from django.test import TestCase
 from django.test.client import Client
@@ -37,3 +40,7 @@ class SimpleTest(TestCase):
         client.post('/accounts/login/', {'username' : 'admin', 'password':'password'})
         response = client.get('')
         self.failUnlessEqual(response.context['form'].fields.keyOrder[0], 'birthDate')
+        
+    def test_command(self):
+        result = print_modles(models.get_app('testMarchenko'))
+        self.failUnlessEqual(string.find(result, "User 1"), 0, "Change db or fail script")

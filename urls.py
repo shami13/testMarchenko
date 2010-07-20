bbd1d91b8c6f1cd0db4b2b3a00e5bf59bb54d3f8
unknown <shami@.(none)>
2010-07-20 23:48:09 +0300

    add ticket 12

--- urls.py
+++ urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from django.conf.urls.defaults import include, patterns
+from django.conf.urls.defaults import include, patterns, url
 
 import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', views.main_page),                   
-    (r'^main/$', views.main_page),
-    (r'^main$', views.main_page),
+    (r'^$', views.main_page),         
+    url(r'^urllist', 'testMarchenko.views.url_list'),          
+    url(r'^main', 'testMarchenko.views.main_page'),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
     (r'^accounts/', include('accounts.urls')),
--- views.py
+++ views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.generic.list_detail import object_list
 
 from forms import UserForm
-from models import User
+from models import User, URL
 
 
 @login_required
@@ -16,3 +17,8 @@ def main_page(request):
         form = UserForm(instance=user)
     form.fields.keyOrder.reverse()
     return render_to_response('main.html', {'form': form}, context_instance=RequestContext(request))
+
+def url_list(request):
+    url_lists = URL.objects.all()[:10]
+    return render_to_response('urllist.html',
+        {'object_list': url_lists}, context_instance=RequestContext(request))

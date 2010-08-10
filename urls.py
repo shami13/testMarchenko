d41e251f34199ac57eab5818ef882f45acb54850
unknown <shami@.(none)>
2010-08-10 22:22:06 +0300

    Add ticket 11

--- urls.py
+++ urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.conf.urls.defaults import include, patterns
 
 import views
+import settings
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', views.main_page),                   
+    (r'^$', views.main_page),
     (r'^main/$', views.main_page),
     (r'^main$', views.main_page),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
     (r'^accounts/', include('accounts.urls')),
+    (r'^admin/jsi18n/', 'django.views.i18n.javascript_catalog'),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
 )
